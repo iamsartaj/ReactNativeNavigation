9dea5fb146a0ae10d270c711b970311ce76fe9b6
iamsartaj <sartajahmadks@gmail.com>
2026-07-10 00:09:28 +0530

    nested navigation, drawer inside stack

--- App.js
+++ App.js
@@ -4,8 +4,18 @@ import { StatusBar } from 'expo-status-bar';
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverViewScreen from './screens/MealsOverView';
 import MealDetailsScreen from './screens/MealDetailsScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Favorites from './screens/Favorites';
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return <Drawer.Navigator>
+    <Drawer.Screen name="Categories" component={CategoriesScreen} options={{ title: "All Categories"}}/>
+    <Drawer.Screen name="Favorites" component={Favorites}/>
+  </Drawer.Navigator>
+}
 
 export default function App() {
   return (
@@ -18,7 +28,7 @@ export default function App() {
           headerTitleStyle: { fontWeight: 'bold' },
           contentStyle: { backgroundColor: '#3f2f25' }
         }}>
-          <Stack.Screen name="Categories" component={CategoriesScreen} options={{ title: "All Categories" }}/>
+          <Stack.Screen name="Drawer" component={DrawerNavigator} options={{ headerShown: false }}/>
           <Stack.Screen name="MealsOverView" component={MealsOverViewScreen} 
             // options={({ route, navigation }) => {
             //   return {
